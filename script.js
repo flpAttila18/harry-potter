f0a9819166703e15c4db917a1ae4e1584a2bf348
tanulo <tanulo@barossgabor.local>
2025-02-17 13:19:31 +0100

    async web

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ window.addEventListener('DOMContentLoaded', fetchCharacters);
 async function fetchCharacters() {
     try {
         const response = await fetch('https://raw.githubusercontent.com/Laboratoria/LIM011-data-lovers/master/src/data/potter/potter.json');
-        console.log(response);
+        //console.log(response);
+        const karakters = await response.json();
+        console.log(karakters);
         
     } catch (error) {
-        
+        console.error("Hiba történt a lekérésben:  ",error);
     }
 }
